13d6d0aff9e41cc7be095c80f641535097a39e81
Jasper Moelker <j.b.moelker@activeids.nl>
2017-09-13 13:04:01 +0200

    Secure Express app with Helmet

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const dataLoader = require('./lib/data-loader')
 const apiRouter = require('./lib/api-router')
 const express = require('express')
+const helmet = require('helmet')
 const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
@@ -15,6 +16,7 @@ const startServer = () => server.listen(port, (err) => {
 	console.log(`> Ready on http://localhost:${port}`);
 });
 
+server.use(helmet())
 server.use('/api', apiRouter)
 server.get('/pages/:slug', (req, res) => app.render(req, res, '/page', { slug: req.params.slug }))
 server.get('*', app.getRequestHandler())
